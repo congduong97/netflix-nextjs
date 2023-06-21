387d3b421c8cc5714b7bdb647f0d456033bd9581
cong.dv <duongcongbka@gmail.com>
2023-06-21 22:57:06 +0700

    add gg analytic

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,18 +1,30 @@
 import Link from "next/link";
 import React from "react";
-
+import { event } from "nextjs-google-analytics";
 export default function home() {
   return (
-    <ul>
-      <li>
-        <Link href="/home">Home</Link>
-      </li>
-      <li>
-        <Link href="/about">About Us</Link>
-      </li>
-      <li>
-        <Link href="/contact">Blog Post</Link>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <Link href="/home">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About Us</Link>
+        </li>
+        <li>
+          <Link href="/contact">Blog Post</Link>
+        </li>
+      </ul>
+      <button
+        onClick={() => {
+          event("submit_form", {
+            category: "Contact",
+            label: "Xin chao toi la cong" + new Date().toDateString(),
+          });
+        }}
+      >
+        Press Home
+      </button>
+    </div>
   );
 }
